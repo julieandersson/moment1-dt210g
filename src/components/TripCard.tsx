13d57f350c5035503a9a3c40a6ca2b0925dbf403
julieandersson <juan2301@student.miun.se>
2025-01-22 22:42:48 +0100

    media queries för resekorten, tar bort inlinestyling på stad i TripCard, sätter en klass på h2 för country, skapar css-fil och minskar storlek vid tablet, media queries för h1 i header också

--- src/components/TripCard.tsx
+++ src/components/TripCard.tsx
@@ -1,3 +1,5 @@
+import '../css/TripCard.css';
+
 interface TripCardsProps {
     country: string,
     city: string,
@@ -8,8 +10,8 @@ interface TripCardsProps {
 function TripCard({country, city, days, visited}: TripCardsProps) {
   return (
     <section style={{backgroundColor: "#dddddd", padding: "25px", borderRadius: "10px", boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2), 0 1px 3px rgba(0, 0, 0, 0.1)" }}>
-    <h2 style={{fontSize: "1.2em"}}>{country}</h2>
-    <p style={{fontSize: "1em"}}>Stad: {city}</p>
+    <h2 style={{fontSize: "1.2em"}} className="trip-card-title">{country}</h2>
+    <p>Stad: {city}</p>
     <p>Antal dagar: {days}</p>
     <p>Besökt: {visited ? "Ja" : "Nej"}</p>
   </section>
