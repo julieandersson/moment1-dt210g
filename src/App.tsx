26bff9dc08498321b18be5326c5b28c6faa7bf3c
julieandersson <juan2301@student.miun.se>
2025-01-22 20:32:02 +0100

    ändrar days till property number istället för string, adderar visited som boolean och sätter Ja som true och Nej till false, uppdaterar värdena i app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,17 @@ import './App.css'
 import TripCard from './components/TripCard'
 
 function App() {
-
   return (
     <>
-    <section>
-    <h1>Resor</h1>
-    <TripCard country={"USA"} city={"Miami"} days={"10"} />
-    <TripCard country={"Thailand"} city={"Phuket"} days={"21"} />
-    <TripCard country={"Madagaskar"} city={"Antananarivo"} days={"30"} />
-    <TripCard country={"Japan"} city={"Tokyo"} days={"14"} />
-    <TripCard country={"Frankrike"} city={"Paris"} days={"7"} />
-    <TripCard country={"Australien"} city={"Sydney"} days={"30"} />
-    </section>
+      <section>
+        <h1>Resor</h1>
+        <TripCard country="USA" city="Miami" days={10} visited={true} />
+        <TripCard country="Thailand" city="Phuket" days={21} visited={true} />
+        <TripCard country="Madagaskar" city="Antananarivo" days={30} visited={false} />
+        <TripCard country="Japan" city="Tokyo" days={14} visited={true} />
+        <TripCard country="Frankrike" city="Paris" days={7} visited={true} />
+        <TripCard country="Australien" city="Sydney" days={30} visited={false} />
+      </section>
     </>
   )
 }
--- src/components/TripCard.tsx
+++ src/components/TripCard.tsx
@@ -1,15 +1,17 @@
 interface TripCardsProps {
     country: string,
     city: string,
-    days: string
+    days: number,
+    visited: boolean;
 }
 
-function TripCard({country, city, days}: TripCardsProps) {
+function TripCard({country, city, days, visited}: TripCardsProps) {
   return (
     <section style={{backgroundColor: "pink", padding: "20px", marginTop: "2em", borderRadius: "25px"}}>
     <h2>{country}</h2>
     <p>Stad: {city}</p>
     <p>Antal dagar: {days}</p>
+    <p>Besökt: {visited ? "Ja" : "Nej"}</p>
   </section>
   )
 }
