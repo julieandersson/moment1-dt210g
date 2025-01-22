95fb5fc8675a060046f4bf5259ec15f874e1acc3
julieandersson <juan2301@student.miun.se>
2025-01-22 20:23:20 +0100

    adderar 5 till värden till propsen, skapar ett interface med värdena country, city och days istället för att skicka ett helt objekt med olika attribut

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ function App() {
     <>
     <section>
     <h1>Resor</h1>
-    <TripCard country={"USA"} city={"New York"} days={"7"} />
-    <TripCard />
-    <TripCard />
+    <TripCard country={"USA"} city={"Miami"} days={"10"} />
+    <TripCard country={"Thailand"} city={"Phuket"} days={"21"} />
+    <TripCard country={"Madagaskar"} city={"Antananarivo"} days={"30"} />
+    <TripCard country={"Japan"} city={"Tokyo"} days={"14"} />
+    <TripCard country={"Frankrike"} city={"Paris"} days={"7"} />
+    <TripCard country={"Australien"} city={"Sydney"} days={"30"} />
     </section>
     </>
   )
--- src/components/TripCard.tsx
+++ src/components/TripCard.tsx
@@ -1,11 +1,15 @@
+interface TripCardsProps {
+    country: string,
+    city: string,
+    days: string
+}
 
-
-function TripCard(props: any) {
+function TripCard({country, city, days}: TripCardsProps) {
   return (
     <section style={{backgroundColor: "pink", padding: "20px", marginTop: "2em", borderRadius: "25px"}}>
-    <h2>{props.country}</h2>
-    <p>Stad: {props.city}</p>
-    <p>Antal dagar: {props.days}</p>
+    <h2>{country}</h2>
+    <p>Stad: {city}</p>
+    <p>Antal dagar: {days}</p>
   </section>
   )
 }
