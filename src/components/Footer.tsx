d011c49fc11b36c1fc65d5f7603b6d7d8dacc4a5
julieandersson <juan2301@student.miun.se>
2025-01-22 21:32:11 +0100

    skapar statiskt innehåll i footern, stylar lite enkelt med css stilmall och inline

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
+import '../css/Footer.css';
+
 function Footer() {
   return (
-    <>
-    
-    </>
+    <footer style={{ textAlign: "center", padding: "10px", marginTop: "20px" }}>
+        <p>&copy; Av Julie Andersson - Kontakta mig via e-post: 
+        <a href="mailto:juan2301@student.miun.se"> juan2301@student.miun.se</a>
+        </p>
+    </footer>
   )
 }
 
