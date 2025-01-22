7bba486fbea3ec6993a247f1620c96b6a64c5b33
julieandersson <juan2301@student.miun.se>
2025-01-22 20:39:58 +0100

    använder en array för att skriva ut alla värden dynamiskt istället för att skriva ut de statiskt

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,53 @@ import './App.css'
 import TripCard from './components/TripCard'
 
 function App() {
+
+  const tripArr = [
+    {
+      country: "USA",
+      city: "Miami",
+      days: 10,
+      visited: true,
+    },
+    {
+      country: "Thailand",
+      city: "Phuket",
+      days: 21,
+      visited: true,
+    },
+    {
+      country: "Madagaskar",
+      city: "Antananarivo",
+      days: 30,
+      visited: false,
+    },
+    {
+      country: "Japan",
+      city: "Tokyo",
+      days: 14,
+      visited: true,
+    },
+    {
+      country: "Frankrike",
+      city: "Paris",
+      days: 7,
+      visited: true,
+    },
+    {
+      country: "Australien",
+      city: "Sydney",
+      days: 30,
+      visited: false,
+    },
+  ]
+  
   return (
     <>
       <section>
         <h1>Resor</h1>
-        <TripCard country="USA" city="Miami" days={10} visited={true} />
-        <TripCard country="Thailand" city="Phuket" days={21} visited={true} />
-        <TripCard country="Madagaskar" city="Antananarivo" days={30} visited={false} />
-        <TripCard country="Japan" city="Tokyo" days={14} visited={true} />
-        <TripCard country="Frankrike" city="Paris" days={7} visited={true} />
-        <TripCard country="Australien" city="Sydney" days={30} visited={false} />
+            {tripArr.map((trip, index) => (
+              <TripCard country={trip.country} city={trip.city} days={trip.days} visited={trip.visited}/>
+            ))}
       </section>
     </>
   )
